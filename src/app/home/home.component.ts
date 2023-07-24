8562ba0cffd446d9bf87af1a186c7b4a0d5de92c
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-07-24 16:13:15 +0200

    NgOptimizedImage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
