548b666d8f7d74efde5be44d7041ad15c731b117
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2023-07-26 17:13:51 +0200

    CD Default Demo

--- src/app/flights/flight-status-toggle/flight-status-toggle.component.ts
+++ src/app/flights/flight-status-toggle/flight-status-toggle.component.ts
@@ -1,15 +1,25 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, inject, Input, Output } from '@angular/core';
+
+import { BlinkService } from '../../shared/blink.service';
 
 @Component({
   selector: 'app-flight-status-toggle',
   standalone: true,
   templateUrl: './flight-status-toggle.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightStatusToggleComponent {
   @Input() status = false;
   @Output() statusChange = new EventEmitter<boolean>();
 
+  private readonly blinkService = inject(BlinkService);
+  private readonly elementRef = inject(ElementRef);
+
   toggle(): void {
     this.statusChange.emit(!this.status);
+  }
+
+  blink(): void {
+    this.blinkService.blinkElementsFirstChild(this.elementRef);
   }
 }
